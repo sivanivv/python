4b7a0af50bc4aa4701be44e9a715090769589f63
sivanivv <sivani2910vv@gmail.com>
2025-06-04 11:31:46 +0530

    updated branch

--- class10.py
+++ class10.py
@@ -234,6 +234,17 @@
 # ------------------------------------------ hourglass pattern
 
 
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n):
+        
+#             if i==0 or i==n-1 or j==0 or j==n-1:
+#                 print("* ",end="  " )
+            
+#             else :
+#                 print("  ",end="  ")
+#     print()
+#--------------------------------------------- hollow square pattern
 
 
 
@@ -241,11 +252,12 @@
 
 
 
-n=5
-for i in range(0,n):
-    for j in range(0,n):
-        if i==n:
-            print("* ",end="  " )
-    for k in range(i+n):
-        print("* ",end="  " )
-    print()
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n):
+#         if i==0 or i==n-1 or j==0 or j==n-1:
+#             print("* ",end="  " )
+        
+#         else :
+#             print("  ",end="  ")
+#     print()
--- systbank.py
+++ systbank.py
@@ -1,17 +1,50 @@
 print('a) Create new account \n','b) Deposit money \n','c) Withdraw money\n','d) Display account details \n ','e) Exit \n',)
-menu=['a','b','c','d','e']
-n=int(input("What is your need? enter option : "))
-# for i in menu:
+
+option=int(input("What is your need? enter option : "))
+data={}
+
+while option!=exit:
+    option=int(input("What is your need? enter option : "))
+    if (option==1):
     
-if (n==1):
-    m=(input('Enter your name '))
-    acc=input('enter account number ')
-    bal=('enter initial balance ')
+        name=(input('Enter your name '))
+        acc=int(input('enter account number '))
     
-    a=['name','account_no','balance']
-    d=[m,acc,bal]
-    # b=list("Enter name , account number and initial balance ")
-    details=zip(d,a)
-    print(details)
-else:
-    print("invalid input : ")
+        bal=int(input('enter initial balance '))
+        details={'name':'','account_no': '','balance':''}
+        
+        
+        details['name']=name
+
+        if acc not in details and acc> 0:
+            details['account_no']=acc
+        # if acc> 0:
+            # details['account_no']=acc
+
+        if bal>0:
+            details['balance']=bal
+
+        for i in acc:
+           
+           data[i]=details
+
+        print(details)
+    # print(data)
+    elif option==2:
+       deposit=int(input("How much money do you want to deposit? "))
+       if deposit>0:
+          bal+=deposit
+          details['balance']=bal
+        #   print(details)
+    elif option==3: 
+       wd=int(input("How much money do you want to withdraw? "))
+       if wd>0 :
+          bal-=wd
+          details['balance']=bal
+        #   print(details)
+    elif option==4:
+       print(details) 
+    elif option==7:
+       print(data) 
+    
+    # print("invalid input : ")
