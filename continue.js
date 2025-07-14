d5524e52e5356c474ceb60e95037aec04bb47291
sivanivv <sivani2910vv@gmail.com>
2025-07-14 11:54:26 +0530

    first commit

---
+++ continue.js
@@ -0,0 +1,30 @@
+
+// for (let i = 0; i < 5; i++) {
+    
+//     if (i==3){
+//         break;
+// }
+// console.log(i)
+// }
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+    
+//     if (i==3){
+//         continue;
+// }
+// console.log(i)
+// }
+
+
+
+let l=5;
+let f=1;
+
+for (let i=1;i<=l;i++){
+    f=f*i
+    
+}
+console.log(f)
