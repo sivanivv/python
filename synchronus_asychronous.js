57cafd5a95591827ad6030d14ee695febc9603b2
sivanivv <sivani2910vv@gmail.com>
2025-07-23 11:19:06 +0530

    first commit

---
+++ synchronus_asychronous.js
@@ -0,0 +1,17 @@
+// console.log("Start");
+// setTimeout(()=>console.log("Middle"),2000);     // excutes middile after 2 seconds
+// console.log("End");
+
+//----------------------------------------------- asynchronous execution
+
+// USING setTimeout
+
+// setTimeout(()=>{
+//   console.log("This runs  after 10  seconds");      
+// },10000);
+
+// setInterval(()=>{
+//  console.log("This runs  after 10  seconds");      
+// },10000);                                    
+
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,3 @@
-let employee=[
-    {name: 'Shein', age: 28},
-    {name: 'Priya', age: 38},
-    {name: 'Merlyn', age: 30},
-]
-let a=employee[0].age;
-// console.log(a);
-for (let i=0;i<employee.length;i++){
-    if (employee[i].age < a){
-        a=employee[i].age 
-        console.log(employee[i])
-    }
-}
+console.log("Start");
+// setTimeout(()=>console.log("Middle"),2000);     // excutes middile after 2 seconds
+// console.log("End");
